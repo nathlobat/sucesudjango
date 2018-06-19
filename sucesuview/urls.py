3ea97fe9e85f268eeb6706d52b7b56b862acb5c5
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-06-19 10:26:39 -0300

    Criação das funções das views

--- sucesuview/urls.py
+++ sucesuview/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('quem_somos/', views.quem_somos, name='quem_somos'),
+    path('diretoria_atual/', views.diretoria_atual, name='diretoria_atual'),
+    path('sucesu_nacional/', views.sucesu_nacional, name='sucesu_nacional'),
+    path('gestoes_anteriotes/', views.gestoes_anteriotes, name='gestoes_anteriotes'),
+    path('estatuto_regimento/', views.estatuto_regimento, name='estatuto_regimento'),
+
+
 ]
--- sucesuview/views.py
+++ sucesuview/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'sucesuview/index.html', {})
+
+def quem_somos(request):
+    return render(request, 'sucesuview/quem_somos.html', {})
+
+def diretoria_atual(request):
+    return render(request, 'sucesuview/diretoria_atual.html', {})
+
+def sucesu_nacional(request):
+    return render(request, 'sucesuview/sucesu_nacional.html', {})
+
+def gestoes_anteriotes(request):
+    return render(request, 'sucesuview/gestoes_anteriotes.html', {})
+
+def estatuto_regimento(request):
+    return render(request, 'sucesuview/estatuto_regimento.html', {})
