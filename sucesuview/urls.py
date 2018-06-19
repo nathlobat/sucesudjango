6ae806d39d083f0bdb2fb3f8404e07b965bb7fbc
LeonCruz <leon.cruz.teixeira@gmail.com>
2018-06-19 10:28:26 -0300

    Correção no nome da view 'gestoes anteriores'

--- sucesuview/urls.py
+++ sucesuview/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('quem_somos/', views.quem_somos, name='quem_somos'),
     path('diretoria_atual/', views.diretoria_atual, name='diretoria_atual'),
     path('sucesu_nacional/', views.sucesu_nacional, name='sucesu_nacional'),
-    path('gestoes_anteriotes/', views.gestoes_anteriotes, name='gestoes_anteriotes'),
+    path('gestoes_anteriores/', views.gestoes_anteriotes, name='gestoes_anteriotes'),
     path('estatuto_regimento/', views.estatuto_regimento, name='estatuto_regimento'),
 
 
