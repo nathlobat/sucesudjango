ca6ffd451390d04b7d1292a6bd1013354bbaeefb
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-25 11:24:12 -0300

    Criação do campo  'hora' na tabela 'Evento'

--- sucesuview/models.py
+++ sucesuview/models.py
@@ -7,5 +7,6 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=200)
     sobre = models.TextField()
     local = models.CharField(max_length=200)
-    data = models.DateTimeField()
+    data = models.DateField(auto_now=False, auto_now_add=False)
+    hora = models.TimeField(auto_now=False, auto_now_add=False, default=None)
     imagem = models.ImageField(upload_to='images/%Y/%m/%d')
