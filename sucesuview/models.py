13cb12df9409c8edebda444a6b6a363df912f4a9
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-25 11:09:38 -0300

    Adição do banco de da função para enviar as imagens do evento

--- sucesuview/models.py
+++ sucesuview/models.py
@@ -2,3 +2,10 @@ from django.db import models
 from django.utils import timezone
 #from sucesuview.models import Post
 
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=200)
+    sobre = models.TextField()
+    local = models.CharField(max_length=200)
+    data = models.DateTimeField()
+    imagem = models.ImageField(upload_to='images/%Y/%m/%d')
