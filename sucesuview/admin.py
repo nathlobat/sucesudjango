676a18dfa237c39b989d322edcb51d85d488627c
Leon Cruz <leon.cruz.teixeira@gmail.com>
2018-07-30 09:21:58 -0300

    Adição das novas tabelas ao admin

--- sucesuview/admin.py
+++ sucesuview/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Evento, DiretoriaAtual
+from .models import Evento, DiretoriaAtual, Assossiados, Parceiros
 
 
 admin.site.register(Evento)
 admin.site.register(DiretoriaAtual)
+admin.site.register(Assossiados)
+admin.site.register(Parceiros)
